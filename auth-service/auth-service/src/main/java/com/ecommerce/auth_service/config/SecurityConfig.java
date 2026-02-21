7dd533f1e44383ab3632033e89290c7c88225631
gangolaabhishek <abhigangola750@gmail.com>
2026-02-21 21:01:58 +0530

    security changes in between

--- auth-service/auth-service/src/main/java/com/ecommerce/auth_service/config/SecurityConfig.java
+++ auth-service/auth-service/src/main/java/com/ecommerce/auth_service/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.ecommerce.auth_service.config;
 
+import com.ecommerce.auth_service.security.JwtAuthenticationFilter;
+import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+
+    @Autowired
+    private JwtAuthenticationFilter jwtFilter;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -22,6 +31,10 @@ public class SecurityConfig {
                                 "/api/auth/login"
                         ).permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
