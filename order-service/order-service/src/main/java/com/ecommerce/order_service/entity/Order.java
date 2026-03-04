715e81e0a79020f735869ea1da273a74287ddb0c
gangolaabhishek <abhigangola750@gmail.com>
2026-03-04 19:44:05 +0530

    order service initial changes

---
+++ order-service/order-service/src/main/java/com/ecommerce/order_service/entity/Order.java
@@ -0,0 +1,71 @@
+package com.ecommerce.order_service.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private double totalAmount;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    public Order() {}
+
+    public Order(Long userId, double totalAmount, String status, LocalDateTime createdAt) {
+        this.userId = userId;
+        this.totalAmount = totalAmount;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
